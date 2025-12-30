b61b5c8fb83fda3834986da628bc9c293edd242c
Aditya Shrivas <as958622@gmail.com>
2025-12-31 00:26:30 +0530

    creating a file session1 and update readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Dsa-Playlist
 In this repository i solved the logic building question from basic to advanced.I solved questions from last few months but i think that i also upload it in my github repo. so i can access it from anywhere eventhough i have to perform any changes in it so it can be easily done.
+
+
+Rules before solve any problem
+1.See problem  2.Understand it   3.Solve 
+
+
+In every file we solve 2-3 basic question initially and after that we'll go to medium questions and then advanced.
---
+++ session1.js
@@ -0,0 +1,23 @@
+
+// Question 1 HOw to print Anything in JS 
+console.log("Aditya");
+
+
+// Question 2 Write a Function that takes two parameters and returns their sum
+ function totalSum(a,b){  // Creating a function
+    return a+b;
+ }
+
+console.log("Total Sum is ",totalSum(5, 10));// Calling the function and printing the result
+
+ 
+
+
+ //Question 3 Calculate the area of a rectangle
+ function areaOfRectangle(len,br){
+    const area = len * br;
+    console.log("Area of Rectangle is ",area);
+    
+ }
+
+    areaOfRectangle(5,10);
